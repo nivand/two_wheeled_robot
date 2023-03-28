65c00fe8820f4db79e6f84d76b53a1d2d7509873
Niv <57392968+nivand@users.noreply.github.com>
2023-03-28 13:19:43 +0330

    new file for dynamic objects added

---
+++ scripts/amcl_pose.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python3
+# Copyright 2021 Samsung Research America
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+from enum import Enum
+
+from action_msgs.msg import GoalStatus
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from lifecycle_msgs.srv import GetState
+from nav2_msgs.action import NavigateThroughPoses, NavigateToPose, FollowWaypoints, ComputePathToPose, ComputePathThroughPoses
+from nav2_msgs.srv import LoadMap, ClearEntireCostmap, ManageLifecycleNodes, GetCostmap
+
+import rclpy
+
+from rclpy.action import ActionClient
+from rclpy.node import Node
+from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy
+from rclpy.qos import QoSProfile
+from math import *
+from numpy import *
+from numpy.linalg import *
+from matplotlib.pyplot import *
+from std_msgs.msg import Float32
+
+class BasicNavigator(Node):
+    def __init__(self):
+        self.t1=time.time()
+        self.t2=time.time()
+        super().__init__(node_name='amcl_pose')
+        self.initial_pose = PoseStamped()
+        self.initial_pose.header.frame_id = 'map'
+        self.cov_x=[]
+        self.i=0.0
+        amcl_pose_qos = QoSProfile(
+          durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+          reliability=QoSReliabilityPolicy.RELIABLE,
+          history=QoSHistoryPolicy.KEEP_LAST,
+          depth=1)
+        self.localization_pose_sub = self.create_subscription(PoseWithCovarianceStamped,
+                                                              'lewis/amcl_pose',
+                                                              self._amclPoseCallback,
+                                                              amcl_pose_qos)
+    def _amclPoseCallback(self, msg):
+        self.debug('Received amcl pose')
+        cov=msg.pose.covariance
+        self.cov_x.append(cov[0,0])
+    
+        
+        return  
+    def plottest(self):
+        
+        
+        dt1=self.t2-self.t1
+        figure()
+        title('covariance')
+        plot(linspace(0.0, dt1, num=len(self.cov_x)),self.cov_x,'b')
+        ylabel('Covariace X($m^2$)')
+        xlabel('Time (s))')
+        grid()  
+
+    def pathplot(self):
+        if self.i==150:
+            self.t2=time.time()
+            self.plottest()
+            show()
+        else:
+            self.i+=1
+            print('i',self.i)  
+def main(args=None):
+    rclpy.init(args=args)
+    m=BasicNavigator()
+    rclpy.spin(m)
+    m.pathplot()
+    show()
+    m.destroy_node()
+    rclpy.shutdown()
+if __name__ == '__main__':
+    main()        
---
+++ scripts/simple_dynamic_obstacle.cpp
@@ -0,0 +1,102 @@
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <gazebo_msgs/srv/set_entity_state.hpp>
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+#include<chrono> 
+#include<random> 
+
+using namespace std; 
+using namespace std::chrono_literals;
+
+class DynamicObstacle : public rclcpp::Node
+{
+    public:
+    DynamicObstacle()
+        : Node("simple_dynamic_obstacle_node")
+    {
+        client = this->create_client<gazebo_msgs::srv::SetEntityState>("/set_entity_state");
+        obs_state.name = "box1";
+        obs_state.pose.position.z = 0.5;
+        obs_state.pose.orientation.x = 0;
+        obs_state.pose.orientation.y = 0;
+        obs_state.pose.orientation.z = 0;
+        obs_state.pose.orientation.w = 1;
+
+        this->declare_parameter<double>("v_x");
+        this->declare_parameter<double>("v_y");
+        this->declare_parameter<std::string>("model_name");
+        this->declare_parameter<double>("min_x");
+        this->declare_parameter<double>("min_y");
+        this->declare_parameter<double>("max_x");
+        this->declare_parameter<double>("max_y");
+
+        this->get_parameter("v_x", vx_);
+        this->get_parameter("v_y", vy_);
+        this->get_parameter("model_name", obs_state.name);
+        this->get_parameter("min_x", minx_);
+        this->get_parameter("min_y", miny_);
+        this->get_parameter("max_x", maxx_);
+        this->get_parameter("max_y", maxy_);
+
+        obs_state.pose.position.x = minx_;
+        obs_state.pose.position.y = miny_;
+
+        timer_ = this->create_wall_timer(
+      50ms, std::bind(&DynamicObstacle::timer_callback, this));
+
+    }
+
+    private:
+    void timer_callback()
+    {  
+        double a,b;
+        a=0.04;
+        b=0.02;
+        vx_ += a*0.05 ;
+        vy_ += b*0.05;
+        obs_state.pose.position.x += vx_*0.05 + 0.5* a *0.0025 ;
+        obs_state.pose.position.y += vy_*0.05 + 0.5* b *0.0025;
+        
+        if (obs_state.pose.position.x > minx_ + 2.0)
+        {
+            a = -0.001;
+        }
+        if (obs_state.pose.position.y > miny_ + 0.7)
+        {
+           
+            b = -0.001;
+        }
+
+        if (!initial_state)
+        {
+            if ((obs_state.pose.position.x < minx_) || (obs_state.pose.position.x > maxx_))
+                vx_ = -vx_;
+                a = -0.01;
+            if ((obs_state.pose.position.y < miny_) || (obs_state.pose.position.y > maxy_))
+                vy_ = -vy_;
+                b = -0.01;
+        }
+        
+        initial_state = false;
+        gazebo_msgs::srv::SetEntityState::Request::SharedPtr state_req = std::make_shared<gazebo_msgs::srv::SetEntityState::Request>();
+        state_req->state = obs_state;
+        auto result = client->async_send_request(state_req);
+        
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Client<gazebo_msgs::srv::SetEntityState>::SharedPtr client;
+    gazebo_msgs::msg::EntityState obs_state;
+    double vx_, vy_, minx_, miny_, maxx_, maxy_ , past ,diff ;
+    bool initial_state = true;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DynamicObstacle>());
+  rclcpp::shutdown();
+  return 0;
+}
